50fe98a125d0bdafe84c34951f8942682f02e896
shemeshg <gili.shemesh@gmail.com>
2025-02-28 14:02:58 +0200

    commit

---
+++ Bal/RtMidiRouterLib/webchannel/wcuserdata.hpp
@@ -0,0 +1,194 @@
+//-define-file body webchannel/wcuserdata.cpp
+//-define-file header webchannel/wcuserdata.h
+//-only-file header //-
+#pragma once
+
+#include <QObject>
+#include <QVariant>
+#include <QJsonObject>
+#include <QStandardPaths>
+#include <QDir>
+#include <QSettings>
+#include "wcmidiin.h"
+#include "wcmidiout.h"
+//- {include-body}
+#include "ApplyConfig.hpp" //- #include "ApplyConfig.h"
+
+//-only-file body //-
+//- #include "wcuserdata.h"
+
+//-only-file header
+//-var {PRE} "WcUserData::"
+class WcUserData : public QObject
+{
+    Q_OBJECT
+public:
+    //- {function} 1 1
+    explicit WcUserData(Webchannel::WcMidiIn *wcmidiin,
+                        Webchannel::WcMidiOut *wcmidiout,
+                        QObject *parent = nullptr)
+    //-only-file body
+        :wcmidiin{wcmidiin},
+        wcmidiout{wcmidiout},
+        QObject(parent)
+    {
+
+
+        QString str;
+        if (isSaveConfigOnServer) {
+            str = cashFileRead(cahedFileName);
+        }
+        if (str.isEmpty()){
+            str = "{}";
+        }
+
+        QByteArray br = str.toUtf8();
+
+        QJsonDocument doc = QJsonDocument::fromJson(br);
+
+        userdata = doc.object();
+
+        if (isSaveConfigOnServer) {
+            applyConfig(doc.object());
+        }
+
+    }
+
+    //- {function} 1 1
+    virtual ~WcUserData()
+    //-only-file body
+    {
+        isSaveConfigOnServer = settings.value("isSaveConfigOnServer", true).toBool();
+        if (isSaveConfigOnServer) {
+            auto o = userdata.toJsonObject();
+            QJsonDocument jsonDoc(o);
+            QString s = jsonDoc.toJson(QJsonDocument::Compact);
+            cashFileWrite(cahedFileName, s);
+
+            qDebug()<<isSaveConfigOnServer;
+            qDebug()<<"Cash path is "<<QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+
+        }
+    }
+
+    //-only-file header
+    QVariant userdata;
+public slots:
+    //- {fn}
+    void setJon(QVariant msg)
+    //-only-file body
+    {
+        userdata = msg;
+        emit userDataChanges(getJon());
+    }
+
+    //- {fn}
+    QVariant getJon()
+    //-only-file body
+    {
+        auto json = userdata.toJsonObject();
+        QStringList inPorts=getMapStringVal(wcmidiin->getPorts());
+        QStringList outPorts=getMapStringVal(wcmidiout->getPorts());
+
+        json["connectedInPorts"] = QJsonArray::fromStringList(inPorts);
+        json["connectedOutPorts"] = QJsonArray::fromStringList(outPorts);
+        userdata = json.toVariantMap();
+        return userdata;
+
+    }
+
+    //- {fn}
+    void applicationQuit()
+    //-only-file body
+    {
+        emit applicationQuitSignal();
+
+    }
+
+    //- {fn}
+    QJsonObject applyConfig(const QJsonObject &json)
+    //-only-file body
+    {
+        ApplyConfig ac(wcmidiin, wcmidiout);
+        auto ret = ac.applyConfig(json);
+        setJon(json);
+        return ret;
+    }
+
+
+
+    //- {fn}
+    void presetOnOff(bool isMidiControlOn, QString presetUuid)
+    //-only-file body
+    {
+        auto json = userdata.toJsonObject();
+        ApplyConfig ac(wcmidiin, wcmidiout);
+        json = ac.presetOnOff(json, isMidiControlOn, presetUuid);
+        applyConfig(json);
+        qDebug()<<"WE ARE AT public slots presetOnOff" <<isMidiControlOn;
+
+    }
+
+    //-only-file header
+signals:
+    bool userDataChanges(QVariant msg);
+    void applicationQuitSignal();
+private:
+    Webchannel::WcMidiIn *wcmidiin;
+    Webchannel::WcMidiOut *wcmidiout;
+
+    //- {fn}
+    QStringList getMapStringVal(QVariantMap map)
+    //-only-file body
+    {
+        QStringList list;
+        for (auto it = map.begin(); it != map.end(); ++it) {
+            list.append(it.value().toString());
+        }
+        return list;
+    }
+
+    //-only-file header
+    const QString cahedFileName = "midiRouterClient.json";
+
+    //- {fn}
+    void cashFileWrite(const QString &fileName, QString &fileContent)
+    //-only-file body
+    {
+        QString cacheFolderPath = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+        QString filePath = cacheFolderPath + QDir::separator() + fileName;
+        QFile file(filePath);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            QTextStream out(&file);
+            out << fileContent;
+            file.close();
+        }
+    }
+
+    //- {fn}
+    QString cashFileRead(const QString &fileName)
+    //-only-file body
+    {
+        QString cacheFolderPath = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+        QString filePath = cacheFolderPath + QDir::separator() + fileName;
+
+        QString fileContent;
+        // Check if the file exists
+        if (QFile::exists(filePath)) {
+            QFile file(filePath);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                QTextStream stream(&file);
+                fileContent = stream.readAll();
+                file.close();
+            }
+        }
+        return fileContent;
+    }
+
+    //-only-file header
+    QSettings settings;
+    bool isSaveConfigOnServer = settings.value("isSaveConfigOnServer", true).toBool();
+
+};
+
+
