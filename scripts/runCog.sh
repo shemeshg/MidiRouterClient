bd4b90b62f6f9003c8ab52971acc1cef9ff3dbce
shemeshg <gili.shemesh@gmail.com>
2025-03-30 08:47:07 +0300

    commit

--- scripts/runCog.sh
+++ scripts/runCog.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-script_dir=$(dirname "$0")
+script_dir=$(dirname $(realpath "$0"))
+
 cd "$script_dir/.."
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
@@ -9,11 +10,19 @@ else
     source .venv/bin/activate
 fi
 
+
+cd "$script_dir/.."
 cd Bal/RtMidiRouterLib/MidiClient/genPrpt
 cog -r *.hpp
 
-cd /Volumes/RAM_Disk_4G/MidiRouterClient/Bal
+cd "$script_dir/.."
+cd Bal
 cog -r BalDataPrivate.h BalDataPrivate.cpp BalData.h BalData.cpp
 
-cd /Volumes/RAM_Disk_4G/MidiRouterClient/Bal/RtMidiRouterLib
+cd "$script_dir/.."
+cd Bal/RtMidiRouterLib
+cog -r CMakeLists.txt
+
+cd "$script_dir/.."
+cd Bal/RtMidiRouterLib/MidiClient
 cog -r CMakeLists.txt
