0033f320f15c38b0b51c1acbbfd3ba75b7736b88
shemeshg <gili.shemesh@gmail.com>
2025-04-24 09:28:26 +0300

    commit

--- Bal/RtMidiRouterLib/MidiClient/genPrpt/property.py
+++ Bal/RtMidiRouterLib/MidiClient/genPrpt/property.py
@@ -1,22 +1,23 @@
 import sys
 import os
+from typing import Optional
 
-def find_scripts_folder(start_path):
-    current_path = start_path
+def find_scripts_folder(start_path: str) -> str:
+    current_path: str = start_path
     while True:
-        scripts_path = os.path.join(current_path, 'scripts')
+        scripts_path: str = os.path.join(current_path, 'scripts')
         if os.path.isdir(scripts_path):
             return scripts_path
-        parent_path = os.path.dirname(current_path)
+        parent_path: str = os.path.dirname(current_path)
         if parent_path == current_path:
             raise FileNotFoundError("scripts folder not found")
         current_path = parent_path
 
 # Get the directory of the current script
-script_dir = os.path.dirname(os.path.realpath(__file__))
+script_dir: str = os.path.dirname(os.path.realpath(__file__))
 
 # Find the scripts folder
-scripts_folder = find_scripts_folder(script_dir)
+scripts_folder: str = find_scripts_folder(script_dir)
 
 # Add the scripts folder to sys.path
 sys.path.append(scripts_folder)
