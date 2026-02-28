809dff5eebf80f01d8de93860eb9d04e1be23933
shemeshg <gili.shemesh@gmail.com>
2026-02-28 14:02:09 +0200

    it is now truelly QtCore Headlesss

--- main.cpp
+++ main.cpp
@@ -1,43 +1,62 @@
+#include <QCoreApplication>
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QIcon>
-#include <QQuickStyle>
-#include "Bal/BalData.h"
 #include <QCommandLineParser>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QIcon>
+#include "Bal/BalData.h"
 
+struct AppConfig {
+    bool headless = false;
+    int port = 0;
+};
 
+AppConfig parseArguments(int argc, char *argv[]) {
+    QCoreApplication tempApp(argc, argv);
 
-
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    BalData bl;
     QCommandLineParser parser;
     parser.addHelpOption();
+
     QCommandLineOption headlessOption(
         QStringList() << "headless",
         "Run in headless mode on the specified port.",
         "port"
         );
+
     parser.addOption(headlessOption);
-    parser.process(app);
+
+    parser.process(tempApp);
+
+    AppConfig cfg;
     if (parser.isSet(headlessOption)) {
+        cfg.headless = true;
         bool ok = false;
-        int port = parser.value(headlessOption).toInt(&ok);
-
-        if (!ok || port <= 0) {
+        cfg.port = parser.value(headlessOption).toInt(&ok);
+        if (!ok || cfg.port <= 0) {
             qWarning() << "Invalid port number for --headless";
-            return 1;
+            exit(1);
         }
-
-        bl.startServer(port);
-        return app.exec();
     }
 
+    return cfg;
+}
+
+int runHeadless(int argc, char *argv[], const AppConfig &cfg) {
+    QCoreApplication app(argc, argv);
+
+    BalData bl;
+    bl.startServer(cfg.port);
+
+    return app.exec();
+}
+
+int runGui(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
     app.setWindowIcon(QIcon(":/icon.png"));
-    #ifdef Q_OS_WIN
+#ifdef Q_OS_WIN
     QQuickStyle::setStyle("Fusion");
-    #endif
+#endif
 
     QQmlApplicationEngine engine;
     QObject::connect(
@@ -45,9 +64,18 @@ int main(int argc, char *argv[])
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+        Qt::QueuedConnection
+        );
+
     engine.loadFromModule("MainQml", "Main");
-
     return app.exec();
+}
 
+int main(int argc, char *argv[]) {
+    AppConfig cfg = parseArguments(argc, argv);
+
+    if (cfg.headless)
+        return runHeadless(argc, argv, cfg);
+    else
+        return runGui(argc, argv);
 }
