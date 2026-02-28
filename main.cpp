e5e7807128cd4ec0c8248075216d1353c7858599
shemeshg <gili.shemesh@gmail.com>
2026-02-28 07:16:54 +0200

    headless option

--- main.cpp
+++ main.cpp
@@ -2,10 +2,38 @@
 #include <QQmlApplicationEngine>
 #include <QIcon>
 #include <QQuickStyle>
+#include "Bal/BalData.h"
+#include <QCommandLineParser>
+
+
+
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    BalData bl;
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    QCommandLineOption headlessOption(
+        QStringList() << "headless",
+        "Run in headless mode on the specified port.",
+        "port"
+        );
+    parser.addOption(headlessOption);
+    parser.process(app);
+    if (parser.isSet(headlessOption)) {
+        bool ok = false;
+        int port = parser.value(headlessOption).toInt(&ok);
+
+        if (!ok || port <= 0) {
+            qWarning() << "Invalid port number for --headless";
+            return 1;
+        }
+
+        bl.startServer(port);
+        return app.exec();
+    }
+
     app.setWindowIcon(QIcon(":/icon.png"));
     #ifdef Q_OS_WIN
     QQuickStyle::setStyle("Fusion");
@@ -21,4 +49,5 @@ int main(int argc, char *argv[])
     engine.loadFromModule("MainQml", "Main");
 
     return app.exec();
+
 }
